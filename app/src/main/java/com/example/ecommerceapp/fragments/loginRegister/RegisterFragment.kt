6fc0eb0821c862e4df198fe13dfc0f76a3e77057
AhmedReda103 <elghostahmed@gmail.com>
2023-02-17 13:26:20 +0200

    Initial commit 1- make design for introduction activity 2- make packages and fragments

---
+++ app/src/main/java/com/example/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerceapp.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommerceapp.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+}
