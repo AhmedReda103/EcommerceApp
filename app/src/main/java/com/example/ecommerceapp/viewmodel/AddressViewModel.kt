ec81cc946ccd190983cd60c79a1ff6c52aca10ae
AhmedReda103 <elghostahmed@gmail.com>
2023-04-06 22:21:23 +0200

    design address fragment_address.xml and and upload user address in firestore

---
+++ app/src/main/java/com/example/ecommerceapp/viewmodel/AddressViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.ecommerceapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ecommerceapp.data.Address
+import com.example.ecommerceapp.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddressViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
+) : ViewModel() {
+
+    private val _addAddress = MutableStateFlow<Resource<Address>>(Resource.Unspecified())
+    val addAddress = _addAddress.asStateFlow()
+
+    private val _error = MutableSharedFlow<String>()
+    val error = _error.asSharedFlow()
+
+    fun addAddress(address: Address) {
+        val validateInputs = validateInputs(address)
+        if(validateInputs){
+            viewModelScope.launch{_addAddress.emit(Resource.Loading())}
+            firestore.collection("user").document(firebaseAuth.uid!!).collection("address").document()
+                .set(address)
+                .addOnSuccessListener {
+                    viewModelScope.launch {
+                        _addAddress.emit(Resource.Success(address))
+                    }
+                }.addOnFailureListener {
+                    viewModelScope.launch {
+                        _addAddress.emit(Resource.Error(it.message.toString()))
+                    }
+                }
+        }else{
+            viewModelScope.launch {
+                _error.emit("All fields are required")
+                //_addAddress.emit(Resource.Error("all fiels are required "))
+            }
+        }
+    }
+
+
+    private fun validateInputs(address: Address): Boolean {
+        return address.addressTitle.trim().isNotEmpty() && address.city.trim()
+            .isNotEmpty() && address.phone.trim().isNotEmpty() && address.state.trim()
+            .isNotEmpty() && address.fullName.trim().isNotEmpty() && address.street.trim().isNotEmpty()
+
+    }
+
+
+}
